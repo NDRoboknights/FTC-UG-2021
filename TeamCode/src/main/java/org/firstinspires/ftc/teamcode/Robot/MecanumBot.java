07243a70bd8d4bc61bbb77c30b41e695a42500af
chiefwithbeef44 <210036@ndhs.org>
2020-11-11 15:44:13 -0800

    tabs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/MecanumBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/MecanumBot.java
@@ -8,40 +8,40 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class MecanumBot extends Bot
 {
 
-    private MotorEx lFMotor;
-    private MotorEx lBMotor;
-    private MotorEx rFMotor;
-    private MotorEx rBMotor;
+	private MotorEx lFMotor;
+	private MotorEx lBMotor;
+	private MotorEx rFMotor;
+	private MotorEx rBMotor;
 
-    public MecanumDrive mecanumDrive;
+	public MecanumDrive mecanumDrive;
 
-    public MecanumBot(HardwareMap hardwareMap)
-    {
-        init(hardwareMap);
-        setZPB(Motor.ZeroPowerBehavior.BRAKE);
-    }
+	public MecanumBot(HardwareMap hardwareMap)
+	{
+		init(hardwareMap);
+		setZPB(Motor.ZeroPowerBehavior.BRAKE);
+	}
 
-    public MecanumBot(HardwareMap hardwareMap, Motor.ZeroPowerBehavior zeroPowerBehavior)
-    {
-        init(hardwareMap);
-        setZPB(zeroPowerBehavior);
-    }
+	public MecanumBot(HardwareMap hardwareMap, Motor.ZeroPowerBehavior zeroPowerBehavior)
+	{
+		init(hardwareMap);
+		setZPB(zeroPowerBehavior);
+	}
 
-    public void init(HardwareMap hardwareMap)
-    {
-        lFMotor = new MotorEx(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312);
-        lBMotor = new MotorEx(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
-        rFMotor = new MotorEx(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312);
-        rBMotor = new MotorEx(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312);
-        mecanumDrive = new MecanumDrive(true, lFMotor, rFMotor, lBMotor, rBMotor);
-    }
+	public void init(HardwareMap hardwareMap)
+	{
+		lFMotor = new MotorEx(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312);
+		lBMotor = new MotorEx(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
+		rFMotor = new MotorEx(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312);
+		rBMotor = new MotorEx(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312);
+		mecanumDrive = new MecanumDrive(true, lFMotor, rFMotor, lBMotor, rBMotor);
+	}
 
-    public void setZPB(Motor.ZeroPowerBehavior zpb)
-    {
-        lFMotor.setZeroPowerBehavior(zpb);
-        rFMotor.setZeroPowerBehavior(zpb);
-        lBMotor.setZeroPowerBehavior(zpb);
-        rBMotor.setZeroPowerBehavior(zpb);
-    }
+	public void setZPB(Motor.ZeroPowerBehavior zpb)
+	{
+		lFMotor.setZeroPowerBehavior(zpb);
+		rFMotor.setZeroPowerBehavior(zpb);
+		lBMotor.setZeroPowerBehavior(zpb);
+		rBMotor.setZeroPowerBehavior(zpb);
+	}
 
 }
